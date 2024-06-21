f8bee499a63a5f53861b27f50aa0ff369fad9575
Emanuel Rodriguez <erodriguez@flowwest.com>
2024-06-20 17:09:15 -0700

    adds more ui elements and styles

--- main.py
+++ main.py
@@ -1,96 +1,115 @@
+from sys import exception
 import boto3
 from dotenv import load_dotenv
 import os
 
+from textual import on
 from textual.app import App, ComposeResult
-from textual.widgets import (
-    Input,
-    Label,
-    Button,
-    Header,
-    Select,
-    SelectionList,
-    ListView,
-    ListItem,
-)
-from textual.containers import Vertical
+from textual.widgets import Input, Button, Header, Pretty, SelectionList, Log
+from textual.containers import Container, Horizontal
+from textual.events import Click
+from textual.validation import Length
 
 
 class BidRunner:
-    def __init__(self, s3_input, s3_output, template):
+    def __init__(self, s3_input):
         self.s3_input = s3_input
-        self.s3_output = s3_output
+        self.aws_is_connected = False
 
-        load_dotenv()
+    def aws_connect(self, access_key, secret_key, session_token):
+        try:
+            self.aws_session = boto3.Session(
+                aws_access_key_id=access_key,
+                aws_secret_access_key=secret_key,
+                aws_session_token=session_token,
+            )
+            self.aws_is_connected = True
+        except Exception as e:
+            print(e)
 
-        aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID")
-        aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
-        aws_session_token = os.getenv("AWS_SESSION_TOKEN")
-
-        self._session = boto3.Session(
-            aws_access_key_id=aws_access_key_id,
-            aws_secret_access_key=aws_secret_access_key,
-            aws_session_token=aws_session_token,
-        )
-        self._s3_session = self._session.resource("s3")
-        self._ec2_client = self._session.client("ec2")
-
-    def ec2_create(self):
-        self._ec2_client.launch_instances()
-
-    def s3_verify(self):
-        self._s3_session.create()
+    def __repr__(self):
+        return f"<BidRunner Input: {'aws connected' if self.aws_is_connected else 'aws NOT connected'}>"
 
 
 # App ---------------------------------------------------
-
-
-class MultiSelectList(ListView):
-    def __init__(self, options):
-        super().__init__()
-        self.options = options
-        self.selected = set()
-        self.build_options()
-
-    def build_options(self):
-        for option in self.options:
-            self.append(ListItem(option))
-
-    def on_list_view_selected(self, event: ListView.Selected) -> None:
-        item = event.node
-        if item in self.selected:
-            self.selected.remove(item)
-            item.remove_class("selected")
-        else:
-            self.selected.add(item)
-            item.add_class("selected")
 
 
 class BidRunnerApp(App):
     CSS_PATH = "styles.tcss"
 
     def on_mount(self) -> None:
-        self.title = "Bidrunner 2"
         load_dotenv()
+        self.title = "Bidrunner2"
 
     def compose(self) -> ComposeResult:
-        BID_MONTHS = ["January", "February", "March", "April", "May", "June"]
-        select_bid_months = ((line, line) for line in BID_MONTHS)
         yield Header()
-        yield Input(placeholder="enter bid id", id="bid-id")
-        yield SelectionList[int](
-            ("Falken's Maze", 0, True),
-            ("Black Jack", 1),
-            ("Gin Rummy", 2),
-            ("Hearts", 3),
-            ("Bridge", 4),
-            ("Checkers", 5),
-            ("Chess", 6, True),
-            ("Poker", 7),
-            ("Fighter Combat", 8, True),
+        yield Horizontal(
+            Container(
+                Input(
+                    placeholder="Bid Name",
+                    id="bid-name",
+                    classes="input-focus",
+                    validators=[Length(minimum=1)],
+                    validate_on=["blur"],
+                ),
+                Input(
+                    placeholder="Input data bucket",
+                    id="bid-input-bucket",
+                    classes="input-focus",
+                    validators=[Length(minimum=1)],
+                    validate_on=["blur"],
+                ),
+                Input(
+                    placeholder="Auction Id",
+                    id="bid-auction-id",
+                    classes="input-focus",
+                    validators=[Length(minimum=1)],
+                    validate_on=["blur"],
+                ),
+                Input(placeholder="enter bid id", id="bid-id", classes="input-focus"),
+                SelectionList[int](
+                    ("January", 1, True),
+                    ("February", 2),
+                    ("March", 3),
+                    ("April", 4),
+                    ("May", 5),
+                    ("June", 6),
+                    id="bid-months",
+                    classes="input-focus",
+                ),
+                Input(
+                    placeholder="enter bid split id",
+                    id="bid-split-id",
+                    classes="input-focus",
+                ),
+                Pretty([]),
+                Button(
+                    "Submit", id="submit_run", variant="default", classes="input-focus"
+                ),
+                id="main-ui",
+            ),
+            Container(Log(), id="log_ui"),
         )
-        yield Input(placeholder="enter bid split id", id="bid-split-id")
-        yield Button("Submit", id="submit-run", variant="default")
+
+    # @on(Input.Changed)
+    # def show_invalid_reasons(self, event: Input.Changed) -> None:
+    #     # Updating the UI to show the reasons why validation failed
+    #     if not event.validation_result.:
+    #         self.query_one(Pretty).update(event.validation_result.failure_descriptions)
+    #     else:
+    #         self.query_one(Pretty).update([])
+
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        log = self.query_one(Log)
+        bid_input_bucket = self.query_one("#bid-input-bucket", Input).value
+        run = BidRunner(bid_input_bucket)
+        run.aws_connect(
+            os.getenv("AWS_ACCESS_KEY_ID"),
+            os.getenv("AWS_SECRET_KEY"),
+            os.getenv("AWS_SESSION_TOKEN"),
+        )
+        if event.button.id == "submit_run":
+            log.write_line(f"Bid Submitted: {run}")
 
 
 if __name__ == "__main__":
