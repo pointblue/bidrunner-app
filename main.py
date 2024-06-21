7d5c4e720009884fad9364361069c23f17078949
Emanuel Rodriguez <erodriguez@flowwest.com>
2024-06-21 01:49:35 -0700

    starting to process Q

--- main.py
+++ main.py
@@ -1,31 +1,87 @@
+from logging import raiseExceptions
 from sys import exception
 import boto3
 from dotenv import load_dotenv
 import os
+import time
 
 from textual import on
 from textual.app import App, ComposeResult
-from textual.widgets import Input, Button, Header, Pretty, SelectionList, Log
+from textual.widgets import Input, Button, Header, SelectionList, Log, Label
 from textual.containers import Container, Horizontal
 from textual.events import Click
 from textual.validation import Length
 
 
 class BidRunner:
-    def __init__(self, s3_input):
+    def __init__(self, s3_input, logger: Log):
         self.s3_input = s3_input
         self.aws_is_connected = False
+        self.logger = logger
+        self.logger.write_line("Welcome to Bidrunner2!")
 
     def aws_connect(self, access_key, secret_key, session_token):
         try:
+            self.logger.write_line("trying to connect to aws")
             self.aws_session = boto3.Session(
                 aws_access_key_id=access_key,
                 aws_secret_access_key=secret_key,
                 aws_session_token=session_token,
+                region_name="us-west-2",
             )
+            s3_client = self.aws_session.client("s3")
+            buckets = s3_client.list_buckets()
             self.aws_is_connected = True
+            self.logger.write_line("connected!")
         except Exception as e:
-            print(e)
+            self.logger.write_line(
+                "unable to connect to aws, with the following error:"
+            )
+            self.logger.write_line(f"{e}")
+
+    def run(self):
+        self.logger.write_line("running bid on cluster")
+        cluster_name = "WaterTrackerDevCluster"
+        task_definition_family = "water-tracker-model-runs"
+        task_definition_revision = "7"
+        task_definition = f"{task_definition_family}:{task_definition_revision}"
+
+        if not self.aws_is_connected:
+            raise Exception("aws needs to be connected")
+
+        cl = self.aws_session.client("ecs")
+        resp = cl.run_task(
+            cluster=cluster_name,
+            taskDefinition=task_definition,
+            count=1,
+            launchType="FARGATE",
+            networkConfiguration={
+                "awsvpcConfiguration": {
+                    "subnets": [
+                        "subnet-00dbf1bd023906da2",
+                        "subnet-0f8ae878792f9ba53",
+                        "subnet-0be2ea73766e5a51a",
+                        "subnet-03d9c808462943df1",
+                    ],  # Replace with your subnet ID
+                    "assignPublicIp": "ENABLED",
+                }
+            },
+        )
+
+    def pollQ(self):
+        sqs = self.aws_session.client("sqs")
+        queue_url = "https://sqs.us-west-2.amazonaws.com/975050180415/water-tracker-Q"
+        while True:
+            response = sqs.receive_message(
+                QueueUrl=queue_url, MaxNumberOfMessages=10, WaitTimeSeconds=20
+            )
+            messages = response.get("Messages", [])
+            for message in messages:
+                self.logger.write_line(message)
+                sqs.delete_message(
+                    QueueUrl=queue_url, ReceiptHandle=message["ReceiptHandle"]
+                )
+            time.sleep(1)
 
     def __repr__(self):
         return f"<BidRunner Input: {'aws connected' if self.aws_is_connected else 'aws NOT connected'}>"
@@ -82,34 +138,43 @@ class BidRunnerApp(App):
                     id="bid-split-id",
                     classes="input-focus",
                 ),
-                Pretty([]),
-                Button(
-                    "Submit", id="submit_run", variant="default", classes="input-focus"
+                Label(id="validation_errors"),
+                Horizontal(
+                    Button(
+                        "Submit",
+                        id="submit_run",
+                        variant="default",
+                        classes="input-focus",
+                    ),
+                    Button(
+                        "Check AWS Connection",
+                        id="submit-aws-connection-check",
+                        variant="warning",
+                        classes="input-focus",
+                    ),
                 ),
                 id="main-ui",
             ),
             Container(Log(), id="log_ui"),
         )
 
-    # @on(Input.Changed)
-    # def show_invalid_reasons(self, event: Input.Changed) -> None:
-    #     # Updating the UI to show the reasons why validation failed
-    #     if not event.validation_result.:
-    #         self.query_one(Pretty).update(event.validation_result.failure_descriptions)
-    #     else:
-    #         self.query_one(Pretty).update([])
-
     def on_button_pressed(self, event: Button.Pressed) -> None:
         log = self.query_one(Log)
         bid_input_bucket = self.query_one("#bid-input-bucket", Input).value
-        run = BidRunner(bid_input_bucket)
-        run.aws_connect(
+        runner = BidRunner(bid_input_bucket, log)
+        runner.aws_connect(
             os.getenv("AWS_ACCESS_KEY_ID"),
-            os.getenv("AWS_SECRET_KEY"),
+            os.getenv("AWS_SECRET_ACCESS_KEY"),
             os.getenv("AWS_SESSION_TOKEN"),
         )
+        if event.button.id == "submit-aws-connection-check":
+            log.write_line(
+                f"Connected to aws? {'Yes' if runner.aws_is_connected else 'No'}"
+            )
         if event.button.id == "submit_run":
-            log.write_line(f"Bid Submitted: {run}")
+            log.write_line(f"Bid Submitted: {runner}")
+            runner.run()
+            runner.pollQ()
 
 
 if __name__ == "__main__":
